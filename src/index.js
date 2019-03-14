96f698dde23931902d1f8e874184801633bcff5b
muhozhuk <ubbodl@gmail.com>
2019-03-14 15:15:45 +0300

    fix: structure

--- src/index.js
+++ src/index.js
@@ -1,39 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
   var i = 0,
     answer;
-  str = str + (bracketsConfig.join('')).replace(/,/g, '');
+  while (i <= bracketsConfig.length - 1) {
+    bracketsConfig[i] = String(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    i++;
+  }
+  i = 0;
   while (i <= str.length) {
-    if (isNaN(str[i])) {
-      switch (str[i] + str[i + 1]) {
-        case "()":
-          str = str.slice(0, i) + str.slice(i + 2, str.length);
-          i--;
-          break;
-        case "[]":
-          str = str.slice(0, i) + str.slice(i + 2, str.length);
-          i--;
-          break;
-        case "{}":
-          str = str.slice(0, i) + str.slice(i + 2, str.length);
-          i--;
-          break;
-        case "||":
-          str = str.slice(0, i) + str.slice(i + 2, str.length);
-          i--;
-          break;
-        default:
-          i++;
-      }
-    } else if (str[i] == str[i + 1]) {
+    if (bracketsConfig.indexOf(str[i] + str[i + 1]) == -1) {
+      i++;
+    } else {
       str = str.slice(0, i) + str.slice(i + 2, str.length);
       i--;
     }
-    else { i++ };
   }
-  if (str === "") {
-    answer = true;
+  if (str==="") {
+    answer=true;
   } else {
-    answer = false;
-  };
+    answer=false;
+  }
   return answer;
 }
