71758f98c0d2294055ed2ecf9e6e296016ab5ea5
muhozhuk <ubbodl@gmail.com>
2019-03-14 12:55:37 +0300

    feat: task solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var i = 0,
+    answer;
+  str = str + (bracketsConfig.join('')).replace(/,/g, '');
+  while (i <= str.length) {
+    if (isNaN(str[i])) {
+      switch (str[i] + str[i + 1]) {
+        case "()":
+          str = str.slice(0, i) + str.slice(i + 2, str.length);
+          i--;
+          break;
+        case "[]":
+          str = str.slice(0, i) + str.slice(i + 2, str.length);
+          i--;
+          break;
+        case "{}":
+          str = str.slice(0, i) + str.slice(i + 2, str.length);
+          i--;
+          break;
+        case "||":
+          str = str.slice(0, i) + str.slice(i + 2, str.length);
+          i--;
+          break;
+        default:
+          i++;
+      }
+    } else if (str[i] == str[i + 1]) {
+      str = str.slice(0, i) + str.slice(i + 2, str.length);
+      i--;
+    }
+    else { i++ };
+  }
+  if (str === "") {
+    answer = true;
+  } else {
+    answer = false;
+  };
+  return answer;
 }
